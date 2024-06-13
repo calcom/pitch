e279f4b1b15e7354ee709a513a7b4c22c70c20b0
Peer Richelsen <peer@cal.com>
2024-06-13 18:15:10 +0200

    added videoask

--- src/components/pitch/section-start.tsx
+++ src/components/pitch/section-start.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export function SectionStart() {
   return (
     <div className="min-h-screen">
@@ -9,6 +11,23 @@ export function SectionStart() {
         <h1 className="text-[110px] font-display bottom-[250px] left-2 md:text-[386px] absolute md:right-0 md:bottom-8 md:left-auto">
           Cal.com
         </h1>
+
+        <Script id="start">
+          {`
+           window.VIDEOASK_EMBED_CONFIG = {
+              kind: "widget",
+              url: "https://www.videoask.com/f32kxm15y",
+              options: {
+                widgetType: "VideoThumbnailWindowTall",
+                text: "Hey 👋",
+                backgroundColor: "#1E1E20",
+                position: "bottom-left",
+                dismissible: false,
+                videoPosition: "center center",
+              },
+            };`}
+        </Script>
+        <Script id="videoask" src="https://www.videoask.com/embed/embed.js" />
       </div>
     </div>
   );
